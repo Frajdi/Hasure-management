265bbcc5b23211eb22ee9d414ee9ffc51d25c0cb
viszefaj <viszefaj8@gmail.com>
2022-06-09 13:53:55 +0000

    message for foreign keys

--- hasura-ecomm/migrations/default/1654776023930_message/up.sql
+++ hasura-ecomm/migrations/default/1654776023930_message/up.sql
@@ -15,3 +15,25 @@ CREATE TABLE IF NOT EXISTS  "public"."message"(
 
 
 );
+
+ALTER TABLE ONLY "public"."message"
+
+ADD CONSTRAINT "message_id_fkey"FOREIGN KEY (chatroom_id) REFERENCES chatroom(id)
+
+ON UPDATE CASCADE
+
+ON DELETE CASCADE
+
+NOT DEFERRABLE;
+
+
+
+ALTER TABLE ONLY "public"."message"
+
+ADD CONSTRAINT "mess_chatroom_id"FOREIGN KEY (user_id) REFERENCES users(id)
+
+ON UPDATE CASCADE
+
+ON DELETE CASCADE
+
+NOT DEFERRABLE;
